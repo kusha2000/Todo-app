3d085cecf78b1da5ce2b357fe77ac91e6c27875b
kusha2000 <kushanandarawewa1@gmail.com>
2023-04-07 11:18:01 +0530

    Design App and Create Database

---
+++ app/src/main/java/com/example/todo_app/EditToDo.java
@@ -0,0 +1,22 @@
+package com.example.todo_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EditToDo extends AppCompatActivity {
+
+    private EditText title,des;
+    private Button edit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_to_do);
+
+        title=findViewById(R.id.editToDoTextTitle);
+        des=findViewById(R.id.editToDoTextDescription);
+        edit=findViewById(R.id.buttonEdit);
+    }
+}
